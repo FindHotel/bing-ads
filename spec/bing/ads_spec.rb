0ea1d1f7378accac935e1cce6f52445668dbbbdd
oss92 <mohamed.o.alnagdy@gmail.com>
2017-08-11 15:06:39 +0200

    Bump version to 0.1.2

--- spec/bing/ads_spec.rb
+++ spec/bing/ads_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Bing::Ads do
   it "has a version number" do
     expect(Bing::Ads::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
