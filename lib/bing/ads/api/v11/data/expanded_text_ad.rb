a1e2d1418460b3a0c73798af73c09d10b854bc59
oss92 <mohamed.o.alnagdy@gmail.com>
2017-08-25 10:33:30 +0200

    Fix ExpandedTextAd fields order

--- lib/bing/ads/api/v11/data/expanded_text_ad.rb
+++ lib/bing/ads/api/v11/data/expanded_text_ad.rb
@@ -9,24 +9,31 @@ module Bing
             # @order
             # https://msdn.microsoft.com/en-us/library/bing-ads-campaign-management-ad.aspx
             KEYS_ORDER = [
+              :ad_format_preference,
               :device_preference,
               :editorial_status,
+              :final_app_urls,
+              :final_mobile_urls,
+              :final_urls,
               :forward_compatibility_map,
               :id,
               :status,
-              :final_urls,
+              :tracking_url_template,
+              :url_custom_parameters,
               :path_1,
               :path_2,
               :text,
               :title_part_1,
-              :title_part_2
+              :title_part_2,
+              :type
             ]
 
             def self.prepare(ad_raw)
               ad_raw['@xsi:type'] = "#{Bing::Ads::API::V11::NAMESPACE_IDENTIFIER}:#{ad_raw[:type]}"
+              # TODO FinalAppUrls
               ad_raw[:final_mobile_urls] = { 'ins1:string' => ad_raw[:final_mobile_urls] } if ad_raw[:final_mobile_urls]
               ad_raw[:final_urls] = { 'ins1:string' => ad_raw[:final_urls] } if ad_raw[:final_urls]
-              # TODO FinalAppUrls
+              ad_raw.delete(:type)
               ad_raw = Bing::Ads::Utils.sort_keys(ad_raw, KEYS_ORDER)
               Bing::Ads::Utils.camelcase_keys(ad_raw)
             end
